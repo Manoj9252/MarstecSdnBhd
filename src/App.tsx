cdde2836292388ac42f84ff3e64ad60ea3242e3a
Maanosh <manoj010503@gmail.com>
2026-04-09 04:20:54 +0800

    Fix base path and router basename for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,9 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/MarstecSdnBhd">
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
@@ -25,4 +24,3 @@ const App = () => (
 );
 
 export default App;
-
